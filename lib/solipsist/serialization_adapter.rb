3176aa77f055279198e08c2084e8d56794048333
Alessandro Decarli <adecarli29@gmail.com>
2025-12-16 17:03:24 +0100

    fix: fixed dynamic gem loading

--- lib/solipsist/serialization_adapter.rb
+++ lib/solipsist/serialization_adapter.rb
@@ -1,19 +1,26 @@
 # frozen_string_literal: true
 
-require_relative 'serialization_strategies/ams_strategy'
-require_relative 'serialization_strategies/jsonapi_strategy'
-
 module Solipsist
   module SerializationAdapter
-    STRATEGIES = {
-      ams: SerializationStrategies::AmsStrategy,
-      jsonapi: SerializationStrategies::JsonapiStrategy
+
+    STRATEGY_PATHS = {
+      ams: 'solipsist/serialization_strategies/ams_strategy',
+      jsonapi: 'solipsist/serialization_strategies/jsonapi_strategy'
+    }.freeze
+
+    STRATEGY_CLASSES = {
+      ams: 'Solipsist::SerializationStrategies::AmsStrategy',
+      jsonapi: 'Solipsist::SerializationStrategies::JsonapiStrategy'
     }.freeze
 
     def self.render_args(model, options = {}, params: {}, &fields_parser)
-      strategy_class = STRATEGIES.fetch(Solipsist.effective_serializer) do
-        raise ArgumentError, "Unknown serializer: #{Solipsist.effective_serializer}"
-      end
+      key = Solipsist.effective_serializer
+      path = STRATEGY_PATHS[key]
+      class_name = STRATEGY_CLASSES[key]
+      raise ArgumentError, "Unknown serializer: #{key}" unless path && class_name
+
+      require path
+      strategy_class = Object.const_get(class_name)
       strategy_class.render_args(model, options, params, &fields_parser)
     end
   end
